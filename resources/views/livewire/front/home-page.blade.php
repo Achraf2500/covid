07cd9264ecb2ef045f330c8127b490d5482bf078
unknown <charafe.boulhout@univ-constantine2.dz>
2021-06-04 03:31:32 +0200

    ui edit

--- resources/views/livewire/front/home-page.blade.php
+++ resources/views/livewire/front/home-page.blade.php
@@ -1,7 +1,26 @@
-<div class="h-screen w-full bg p-4 overflow-hidden">
-    <nav>
-        <div>
-            <img src="./assets/images/logo.svg" alt="">
-        </div>
-    </nav>
+<div class="h-screen w-full bg p-4 overflow-hidden font-base">
+    <div class="w-11/12 mx-auto h-full">
+        <nav class="flex items-center justify-between">
+            <div class="w-32">
+                <img src="./assets/images/logo.svg" alt="" draggable="false">
+            </div>
+            <div>
+                <ul class="flex items-center text-white uppercase text-base">
+                    <li class="mx-2 cursor-pointer"><a href="">Home</a></li>
+                    <li class="mx-2 cursor-pointer"><a href="">coronavirus ?</a></li>
+                    <li class="mx-2 cursor-pointer"><a href="">health patient ?</a></li>
+                    <li class="mx-2 cursor-pointer"><a href="">à propos de</a></li>
+                    <li class="mx-2 cursor-pointer"><a href="">contact</a></li>
+                    <li class="mx-2 cursor-pointer p-3 w-32 text-center bg-yellow-400 rounded-br-md rounded-tl-lg hover:shadow-md"><a href="">connexion</a></li>
+                </ul>
+            </div>
+        </nav>
+        <section class="flex justify-between  h-full">
+            <div class="w-1/2 flex items-center justify-center">
+                <div class="text-white font-ubuntu text-8xl">Covid 19 <span class="text-yellow-400 text-2xl">Coronavirus</span></div>
+                <p></p>
+            </div>
+            <div class="w-1/2 pt-4"><img src="./assets/images/nurse.png" class="mx-auto w-4/5" draggable="false" alt="" srcset=""></div>
+        </section>
+    </div>
 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,12 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'ubuntu': ['Squada One','cursive'],
+        'base': ['Bree Serif','sans-serif']
+      }
+    },
   },
   variants: {
     extend: {},
